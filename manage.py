2359345aff50a115e887fdb384f369b6115068fe
ZJDTop <154469562@qq.com>
2019-11-20 20:59:44 +0800

    添加了shopcararAPI

--- manage.py
+++ manage.py
@@ -1,14 +1,11 @@
-# !/usr/bin/env python
-# -*-coding:UTF-8-*-
-# __author__ = pighui
-# __time__ = 2019-11-19 下午6:51
 from flask_cors import CORS
 
 from mainapp import app
 from settings import HOST, PORT
 from mainapp.views import user_api
-
+from shopapp.views import shopcar_api
 
 app.register_blueprint(user_api.user_blue, url_prefix='/user')
+app.register_blueprint(shopcar_api.shopcar_blue, url_prefix='/cart')
 CORS(app)
 app.run(host=HOST, port=PORT)
