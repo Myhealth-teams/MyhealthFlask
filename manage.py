57bb27c7db4742b26699862d99027959171931ac
xuhui <pighui233@163.com>
2019-11-20 16:56:59 +0800

    添加跨域

--- manage.py
+++ manage.py
@@ -2,10 +2,13 @@
 # -*-coding:UTF-8-*-
 # __author__ = pighui
 # __time__ = 2019-11-19 下午6:51
+from flask_cors import CORS
+
 from mainapp import app
 from settings import HOST, PORT
 from mainapp.views import user_api
 
 
 app.register_blueprint(user_api.user_blue, url_prefix='/user')
+CORS(app)
 app.run(host=HOST, port=PORT)
