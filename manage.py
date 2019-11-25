a0ee68c091a57f006e20df12e8f09c8fd6f845b0
xuhui <pighui233@163.com>
2019-11-25 15:56:27 +0800

    新增猴子补丁和进程

--- manage.py
+++ manage.py
@@ -1,15 +1,25 @@
+from gevent import monkey
+monkey.patch_all()
 from flask_cors import CORS
-
 from goodsapp.views import goods_api
 from homeapp.views import home_api
 from mainapp import app
 from settings import HOST, PORT
 from mainapp.views import user_api
 from shopapp.views import shopcar_api
+from multiprocessing import Process
 
-app.register_blueprint(user_api.user_blue, url_prefix='/user')
-app.register_blueprint(goods_api.goods_blue, url_prefix='/goods')
-app.register_blueprint(home_api.home_blue, url_prefix='/home')
-app.register_blueprint(shopcar_api.shopcar_blue, url_prefix='/cart')
-CORS(app)
-app.run(host=HOST, port=PORT)
+
+
+
+def run():
+    app.register_blueprint(user_api.user_blue, url_prefix='/user')
+    app.register_blueprint(goods_api.goods_blue, url_prefix='/goods')
+    app.register_blueprint(home_api.home_blue, url_prefix='/home')
+    app.register_blueprint(shopcar_api.shopcar_blue, url_prefix='/cart')
+    CORS(app)
+    app.run(host=HOST, port=PORT)
+
+if __name__ == '__main__':
+    process = Process(target=run())
+    process.start()
