745dcb5bd834a21e89ad4e89c5a3672068c773a5
xuhui <pighui233@163.com>
2019-11-27 20:00:25 +0800

    修改部署文件

--- run.sh
+++ run.sh
@@ -1,7 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 echo 'starting MyhealthFlask project'
 cd /usr/src/MyhealthFlask
-git pull
-pip install -r requirements.txt
-cd /usr/src/MyhealthFlask/
 gunicorn -w 1 -b 0.0.0.0:5000 -k gevent manage:application -D
