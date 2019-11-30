03972811ca2de36662596ec321dd0ea131228765
xuhui <pighui233@163.com>
2019-11-30 14:45:56 +0800

    修改模型

--- models.py
+++ models.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm import relationship
 from sqlalchemy.schema import FetchedValue
 from sqlalchemy.ext.declarative import declarative_base
 
+
 Base = declarative_base()
 metadata = Base.metadata
 
@@ -20,9 +21,9 @@ class Authdeatil(Base):
 class Cart(Base):
     __tablename__ = 'carts'
 
-    c_id = Column(Integer, primary_key=True)
-    u_id = Column(ForeignKey('users.id'), index=True)
-    goods_id = Column(ForeignKey('goods.goods_id'), index=True)
+    c_id = Column(Integer, primary_key=True, unique=True)
+    u_id = Column(ForeignKey('users.id'), nullable=False, index=True)
+    goods_id = Column(ForeignKey('goods.goods_id'), nullable=False, index=True)
     c_goods_num = Column(Integer, nullable=False)
     c_is_selected = Column(Integer, nullable=False)
 
@@ -34,10 +35,8 @@ class City(Base):
     __tablename__ = 'city'
 
     cityid = Column(Integer, primary_key=True, unique=True)
-    provinceid = Column(ForeignKey('province.provinceid'), index=True)
-    cityname = Column(String(45))
-
-    province = relationship('Province', primaryjoin='City.provinceid == Province.provinceid', backref='cities')
+    provinceid = Column(Integer, nullable=False)
+    cityname = Column(String(45), nullable=False)
 
 
 class DiscountGood(Base):
@@ -46,10 +45,9 @@ class DiscountGood(Base):
     dg_id = Column(Integer, primary_key=True, unique=True)
     goods_id = Column(ForeignKey('goods.goods_id'), nullable=False, index=True)
     discount = Column(Float, nullable=False)
-    new_price = Column(Float)
+    new_price = Column(Float, nullable=False)
 
-    goods = relationship('Good', lazy="immediate", primaryjoin='DiscountGood.goods_id == Good.goods_id',
-                         backref='discount_goods')
+    goods = relationship('Good', lazy="immediate", primaryjoin='DiscountGood.goods_id == Good.goods_id', backref='discount_goods')
 
 
 class DjangoMigration(Base):
@@ -65,11 +63,11 @@ class Doctor(Base):
     __tablename__ = 'doctors'
 
     d_id = Column(Integer, primary_key=True, unique=True)
-    room_id = Column(ForeignKey('room.room_id'), index=True)
-    d_name = Column(String(20))
-    d_relname = Column(String(20))
-    d_skill = Column(String(1000))
-    d_head = Column(String(200))
+    room_id = Column(ForeignKey('room.room_id'), nullable=False, index=True)
+    d_name = Column(String(20), nullable=False)
+    d_relname = Column(String(20), nullable=False)
+    d_skill = Column(String(1000), nullable=False)
+    d_head = Column(String(200), nullable=False)
     d_idcard = Column(String(20))
     d_cer = Column(String(256))
     is_order = Column(Integer, nullable=False, server_default=FetchedValue())
@@ -77,20 +75,12 @@ class Doctor(Base):
     room = relationship('Room', primaryjoin='Doctor.room_id == Room.room_id', backref='doctors')
 
 
-class Explain(Base):
-    __tablename__ = 'explain'
-
-    id = Column(Integer, primary_key=True)
-    english = Column(String(20), nullable=False)
-    chinese = Column(String(20), nullable=False)
-
-
 class FollowDoc(Base):
     __tablename__ = 'follow_doc'
 
     fd_id = Column(Integer, primary_key=True, unique=True)
-    u_id = Column(ForeignKey('users.id'), index=True)
-    d_id = Column(ForeignKey('doctors.d_id'), index=True)
+    u_id = Column(ForeignKey('users.id'), nullable=False, index=True)
+    d_id = Column(ForeignKey('doctors.d_id'), nullable=False, index=True)
 
     d = relationship('Doctor', lazy="immediate", primaryjoin='FollowDoc.d_id == Doctor.d_id', backref='follow_docs')
     u = relationship('User', primaryjoin='FollowDoc.u_id == User.id', backref='follow_docs')
@@ -100,41 +90,51 @@ class FollowGood(Base):
     __tablename__ = 'follow_good'
 
     fd_id = Column(Integer, primary_key=True, unique=True)
-    u_id = Column(ForeignKey('users.id'), index=True)
-    goods_id = Column(ForeignKey('goods.goods_id'), index=True)
+    u_id = Column(ForeignKey('users.id'), nullable=False, index=True)
+    goods_id = Column(ForeignKey('goods.goods_id'), nullable=False, index=True)
 
-    goods = relationship('Good', lazy="immediate", primaryjoin='FollowGood.goods_id == Good.goods_id',
-                         backref='follow_goods')
+    goods = relationship('Good', lazy="immediate", primaryjoin='FollowGood.goods_id == Good.goods_id', backref='follow_goods')
     u = relationship('User', primaryjoin='FollowGood.u_id == User.id', backref='follow_goods')
+
+
+class Forum(Base):
+    __tablename__ = 'forum'
+
+    f_id = Column(Integer, primary_key=True, unique=True)
+    u_id = Column(ForeignKey('users.id'), nullable=False, index=True)
+    f_title = Column(String(100), nullable=False)
+    f_content = Column(String(1000), nullable=False)
+
+    u = relationship('User', lazy="immediate", primaryjoin='Forum.u_id == User.id', backref='forums')
 
 
 class Good(Base):
     __tablename__ = 'goods'
 
-    goods_id = Column(Integer, primary_key=True)
-    goods_name = Column(String(50))
-    url = Column(String(256))
-    price = Column(Float)
-    medtype = Column(Integer)
-    standards = Column(String(30))
-    detail = Column(String(4096))
-    product_num = Column(String(20))
-    product_name = Column(String(20))
-    normal_name = Column(String(50))
-    allow_num = Column(String(20))
-    imgs = Column(String(512))
-    introduce_img = Column(String(100))
+    goods_id = Column(Integer, primary_key=True, unique=True)
+    goods_name = Column(String(50), nullable=False)
+    url = Column(String(256), nullable=False)
+    price = Column(Float, nullable=False)
+    medtype = Column(Integer, nullable=False)
+    standards = Column(String(30), nullable=False)
+    detail = Column(String(256), nullable=False)
+    product_num = Column(String(20), nullable=False)
+    product_name = Column(String(20), nullable=False)
+    normal_name = Column(String(50), nullable=False)
+    allow_num = Column(String(20), nullable=False)
+    imgs = Column(String(512), nullable=False)
+    introduce_img = Column(String(100), nullable=False)
 
 
 class Hospital(Base):
     __tablename__ = 'hospitals'
 
     h_id = Column(Integer, primary_key=True, unique=True)
-    cityid = Column(ForeignKey('city.cityid'), index=True)
-    hname = Column(String(50))
-    haddress = Column(String(100))
-    hgrade = Column(String(20))
-    htext = Column(String(200))
+    cityid = Column(ForeignKey('city.cityid'), nullable=False, index=True)
+    hname = Column(String(50), nullable=False)
+    haddress = Column(String(100), nullable=False)
+    hgrade = Column(String(20), nullable=False)
+    htext = Column(String(200), nullable=False)
 
     city = relationship('City', primaryjoin='Hospital.cityid == City.cityid', backref='hospitals')
 
@@ -143,25 +143,36 @@ class Infomation(Base):
     __tablename__ = 'infomation'
 
     i_id = Column(Integer, primary_key=True, unique=True)
-    titile = Column(String(50))
-    content = Column(String(10000))
+    titile = Column(String(50), nullable=False)
+    content = Column(String(10000), nullable=False)
 
 
 class Medtype(Base):
     __tablename__ = 'medtypes'
 
-    m_id = Column(Integer, primary_key=True)
+    m_id = Column(Integer, primary_key=True, unique=True)
     typenum = Column(Integer, nullable=False)
     medname = Column(String(50), nullable=False)
+
+
+class Notice(Base):
+    __tablename__ = 'notice'
+
+    n_id = Column(Integer, primary_key=True, unique=True)
+    s_id = Column(ForeignKey('sys_user.id'), nullable=False, index=True)
+    n_text = Column(String(100), nullable=False)
+    is_use = Column(Integer, nullable=False, server_default=FetchedValue())
+
+    s = relationship('SysUser', primaryjoin='Notice.s_id == SysUser.id', backref='notices')
 
 
 class Orderdetail(Base):
     __tablename__ = 'orderdetail'
 
     o_detail_id = Column(Integer, primary_key=True, unique=True)
-    o_id = Column(ForeignKey('orderlist.o_id'), index=True)
-    goods_id = Column(ForeignKey('goods.goods_id'), index=True)
-    goods_num = Column(Integer)
+    o_id = Column(ForeignKey('orderlist.o_id'), nullable=False, index=True)
+    goods_id = Column(ForeignKey('goods.goods_id'), nullable=False, index=True)
+    goods_num = Column(Integer, nullable=False)
 
     goods = relationship('Good', primaryjoin='Orderdetail.goods_id == Good.goods_id', backref='orderdetails')
     o = relationship('Orderlist', primaryjoin='Orderdetail.o_id == Orderlist.o_id', backref='orderdetails')
@@ -171,30 +182,31 @@ class Orderlist(Base):
     __tablename__ = 'orderlist'
 
     o_id = Column(Integer, primary_key=True, unique=True)
-    o_identifier = Column(String(100), unique=True)
-    u_id = Column(ForeignKey('users.id'), index=True)
-    o_price = Column(Float)
-    o_time = Column(DateTime)
-    o_nums = Column(Integer)
-    a_id = Column(ForeignKey('user_address.a_id'), index=True)
-    o_state = Column(Integer)
+    o_identifier = Column(String(100), nullable=False, unique=True)
+    u_id = Column(ForeignKey('users.id'), nullable=False, index=True)
+    o_price = Column(Float, nullable=False)
+    o_time = Column(DateTime, nullable=False)
+    o_nums = Column(Integer, nullable=False)
+    a_id = Column(ForeignKey('user_address.a_id'), nullable=False, index=True)
+    o_state = Column(Integer, nullable=False)
 
     a = relationship('UserAddres', primaryjoin='Orderlist.a_id == UserAddres.a_id', backref='orderlists')
     u = relationship('User', primaryjoin='Orderlist.u_id == User.id', backref='orderlists')
+
 
 class Province(Base):
     __tablename__ = 'province'
 
     provinceid = Column(Integer, primary_key=True, unique=True)
-    provincename = Column(String(20))
+    provincename = Column(String(20), nullable=False)
 
 
 class Room(Base):
     __tablename__ = 'room'
 
     room_id = Column(Integer, primary_key=True, unique=True)
-    h_id = Column(ForeignKey('hospitals.h_id'), index=True)
-    roomname = Column(String(45))
+    h_id = Column(ForeignKey('hospitals.h_id'), nullable=False, index=True)
+    roomname = Column(String(45), nullable=False)
 
     h = relationship('Hospital', primaryjoin='Room.h_id == Hospital.h_id', backref='rooms')
 
@@ -210,7 +222,7 @@ class RotationSelect(Base):
 class Rotatiton(Base):
     __tablename__ = 'rotatitons'
 
-    ro_id = Column(Integer, primary_key=True)
+    ro_id = Column(Integer, primary_key=True, unique=True)
     url = Column(String(256), nullable=False)
 
 
@@ -240,22 +252,64 @@ class SysUserRole(Base):
     role_id = Column(Integer)
 
 
+class Translate(Base):
+    __tablename__ = 'translate'
+
+    id = Column(Integer, primary_key=True)
+    english = Column(String(20), nullable=False)
+    chinese = Column(String(20), nullable=False)
+
+
 class UserAddres(Base):
     __tablename__ = 'user_address'
 
     a_id = Column(Integer, primary_key=True, unique=True)
-    id = Column(ForeignKey('users.id'), index=True)
-    provinceid = Column(ForeignKey('province.provinceid'), index=True)
-    cityid = Column(ForeignKey('city.cityid'), index=True)
-    user_name = Column(String(20))
-    user_tel = Column(String(11))
-    detail_address = Column(String(200))
-    is_default = Column(Integer)
+    id = Column(ForeignKey('users.id'), nullable=False, index=True)
+    provinceid = Column(ForeignKey('province.provinceid'), nullable=False, index=True)
+    cityid = Column(ForeignKey('city.cityid'), nullable=False, index=True)
+    user_name = Column(String(20), nullable=False)
+    user_tel = Column(String(11), nullable=False)
+    detail_address = Column(String(200), nullable=False)
+    is_default = Column(Integer, nullable=False)
 
     city = relationship('City', primaryjoin='UserAddres.cityid == City.cityid', backref='user_address')
     user = relationship('User', primaryjoin='UserAddres.id == User.id', backref='user_address')
-    province = relationship('Province', primaryjoin='UserAddres.provinceid == Province.provinceid',
-                            backref='user_address')
+    province = relationship('Province', primaryjoin='UserAddres.provinceid == Province.provinceid', backref='user_address')
+
+
+class UserInfo(Base):
+    __tablename__ = 'user_info'
+
+    ui_id = Column(Integer, primary_key=True, unique=True)
+    u_id = Column(ForeignKey('users.id'), nullable=False, index=True)
+    u_height = Column(Float, nullable=False)
+    u_weight = Column(Float, nullable=False)
+    u_sex = Column(Integer, nullable=False)
+    u_relname = Column(String(50))
+
+    u = relationship('User', primaryjoin='UserInfo.u_id == User.id', backref='user_infos')
+
+
+class UserNotice(Base):
+    __tablename__ = 'user_notice'
+
+    un_id = Column(Integer, primary_key=True, unique=True)
+    u_id = Column(ForeignKey('users.id'), nullable=False, index=True)
+    un_titile = Column(String(50), nullable=False)
+    un_text = Column(String(100), nullable=False)
+    un_time = Column(String(10), nullable=False)
+
+    u = relationship('User', lazy="immediate", primaryjoin='UserNotice.u_id == User.id', backref='user_notices')
+
+
+class UserPaypal(Base):
+    __tablename__ = 'user_paypal'
+
+    up_id = Column(Integer, primary_key=True, unique=True)
+    u_id = Column(ForeignKey('users.id'), nullable=False, unique=True)
+    up_balance = Column(Float, nullable=False, server_default=FetchedValue())
+
+    u = relationship('User', primaryjoin='UserPaypal.u_id == User.id', backref='user_paypals')
 
 
 class Userauth(Base):
@@ -270,63 +324,8 @@ class Userauth(Base):
 class User(Base):
     __tablename__ = 'users'
 
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, unique=True)
     u_name = Column(String(20))
     u_password = Column(String(50), nullable=False)
     u_tel = Column(String(11), nullable=False, unique=True)
     u_image = Column(String(256))
-
-
-
-class UserInfo(Base):
-    __tablename__ = 'user_info'
-
-    ui_id = Column(Integer, primary_key=True, unique=True)
-    u_id = Column(ForeignKey('users.id'), nullable=False, index=True)
-    u_height = Column(Float, nullable=False)
-    u_weight = Column(Float, nullable=False)
-    u_sex = Column(Integer, nullable=False)
-    u_relname = Column(String(50), nullable=True)
-
-    u = relationship('User', primaryjoin='UserInfo.u_id == User.id', backref='user_infos')
-
-
-class Forum(Base):
-    __tablename__ = 'forum'
-
-    f_id = Column(Integer, primary_key=True, unique=True)
-    u_id = Column(ForeignKey('users.id'), index=True)
-    f_title = Column(String(100))
-    f_content = Column(String(1000))
-
-    u = relationship('User', lazy="immediate", primaryjoin='Forum.u_id == User.id', backref='forums')
-
-class Notice(Base):
-    __tablename__ = 'notice'
-
-    n_id = Column(Integer, primary_key=True, unique=True)
-    s_id = Column(ForeignKey('sys_user.id'), index=True)
-    n_text = Column(String(100))
-    is_use = Column(Integer, nullable=False, server_default=FetchedValue())
-
-    s = relationship('SysUser', primaryjoin='Notice.s_id == SysUser.id', backref='notices')
-
-class UserNotice(Base):
-    __tablename__ = 'user_notice'
-
-    un_id = Column(Integer, primary_key=True, unique=True)
-    u_id = Column(ForeignKey('users.id'), nullable=False, index=True)
-    un_titile = Column(String(50), nullable=False)
-    un_text = Column(String(100), nullable=False)
-    un_time = Column(String(10), nullable=False)
-
-    u = relationship('User', lazy="immediate", primaryjoin='UserNotice.u_id == User.id', backref='user_notices')
-
-class UserPaypal(Base):
-    __tablename__ = 'user_paypal'
-
-    up_id = Column(Integer, primary_key=True, unique=True)
-    u_id = Column(ForeignKey('users.id'), unique=True)
-    up_balance = Column(Float, server_default=FetchedValue())
-
-    u = relationship('User', primaryjoin='UserPaypal.u_id == User.id', backref='user_paypals')
