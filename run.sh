eddc8439af1f688ae3c07972c1ddfc532e3cbd88
xuhui <pighui233@163.com>
2019-12-02 13:15:34 +0800

    改回去了

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 echo 'starting MyhealthFlask project'
 cd /usr/src/MyhealthFlask
-gunicorn -w 1 -b 0.0.0.0:5000 -k gevent manage:application
+gunicorn -w 1 -b 0.0.0.0:5000 manage:application
